882546420a82cafcfe7ec8d9c0bb0ad22657b38b
avflett <aflett3@academic.rrc.ca>
2025-01-31 22:02:07 -0600

    Experiemented with built-in functions.

--- READ.md
+++
@@ -1,7 +0,0 @@
-# This is my demo for Module 2
-
-## Description
-I am using this demo as practice so I can learn what I need to for the Module 2 Assignment and Quiz.
-
-# Author
-Amber FLett
--- mod_2_demo.py
+++ mod_2_demo.py
@@ -1,1 +1,30 @@
 """A module to Demonstrate content from Module 2."""
+
+my_name = "Amber"
+my_age = 25
+print("My name is", my_name, "and I am", my_age, "years old")
+print(f"My name is {my_name} and I am {my_age} years old")
+absolute_value = abs(-12)
+print("absolute_value:", absolute_value)
+
+result = 1 * 3
+print("result:", result)
+print(f"result: {result}")
+
+print("apples", "oranges", "bananas")
+print("apples", "oranges", "bananas", sep = ", ")
+
+value = 3.14959
+print(f"The value is {value:.2f}.")
+
+number = 123
+
+print(f"The number is {number:05}.")
+print(f"The number is {number:5}.")
+
+name = "Amber"
+print(f"Hello, {name:>10}.")
+print(f"Hello, {name:10}.")
+
+print("Hello\tWorld\nHi\tWorld")
+print("Hello\v\vWorld")
